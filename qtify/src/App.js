6175e33e603fc30e4558d7bf4898a042dd4178eb
Soumya Ranjan Sahoo <luckyssoumya719@gmail.com>
2025-11-17 14:16:38 +0530

    Adding Circular image - to test the time required to move from dev to prod

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.pixabay.com/photo/2022/04/09/14/44/circular-7121578_1280.png" className="App-logo" alt="logo" />
         <p>
           Soumya's Portfolio website is coming soon -Check part 2
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
