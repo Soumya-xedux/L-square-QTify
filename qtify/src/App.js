f022df0adad4ba0140781630512d52190fe50ccd
Soumya Ranjan Sahoo <luckyssoumya719@gmail.com>
2025-11-17 14:37:28 +0530

    successfuly validated www Host, wait for @ Host

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src="https://cdn.pixabay.com/photo/2022/04/09/14/44/circular-7121578_1280.png" className="App-logo" alt="logo" />
         <p>
-          Soumya's Portfolio website is coming soon -Check part 2
+          www.sahootech.app - Successfully Established
+          Waiting for sahootech.app
         </p>
         
       </header>
